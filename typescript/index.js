600a840d711fe16800c15959d08538a554157bc5
Arturas <a.molcanovas@gmail.com>
2020-09-20 22:19:38 +0100

    add typescript plugin extensions

--- typescript/index.js
+++ typescript/index.js
@@ -1,6 +1,10 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   rules: {
     'array-type': ['error', 'array-simple'],
     '@typescript-eslint/ban-ts-comment': 'off',
