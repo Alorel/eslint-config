c8a453293266b4616a79c09710357fc4e50793bd
Arturas <a.molcanovas@gmail.com>
2020-09-22 16:08:06 +0100

    fix(base): Tweak space-before-function-paren rule

--- base/index.js
+++ base/index.js
@@ -246,7 +246,11 @@ module.exports = {
       }
     ],
     'space-before-blocks': 'error',
-    'space-before-function-paren': ['error', 'never'],
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }],
     'space-in-parens': 'error',
     'space-infix-ops': 'error',
     'space-unary-ops': [
